af45efb62f7fffb1a6907e0c667cc73c4ec9b850
imvarunkmr <imvarunkmr@gmail.com>
2016-01-12 18:56:39 +0530

    Refactored code for better integration

--- leads/classes/class-lf-lead-capture.php
+++ leads/classes/class-lf-lead-capture.php
@@ -20,38 +20,61 @@ class LF_Lead_Capture {
 		else
 			$name = 'Not Available';
 
-		$source = isset( $lead['source'] ) ? $lead['source'] : "3rd Party" ;
-		$form = array(
-			'reference' => isset( $lead['form_id'] ) ? $lead['form_id'] : 'Not Available',
-			'source' => $source,
-			'provider' => $lead['provider'],
-		);
-
-		$properties = array(
-			'name' => $name,
-			'email' => isset( $lead['email'] ) ? $lead['email'] : 'Not Available',
-		);
-
 		$data = array(
 			'content' => array( 
-				'form' => $form,
-				'properties' => $properties,
-				'visitor' => '<cookie_value>',
-				'session' => '<cookie_value>'
-			 ),
+				'form' => array(
+					'reference' => isset( $lead['form_id'] ) ? $lead['form_id'] : 'Not Available',
+					'source' => isset( $lead['source'] ) ? $lead['source'] : "3rd Party",
+					'provider' => $lead['provider'],
+				),
+			),
+			'properties' => array(
+				'name' => $name,
+				'email' => isset( $lead['email'] ) ? $lead['email'] : 'Not Available',
+			),
 		);
 
-		$json_data = json_encode( $data );
+		foreach(array('visitor','session',) as $cookie) {
+			if(isset($_COOKIE[LF_PREFIX . $cookie])) {
+				$data[$cookie] = $_COOKIE[LF_PREFIX . $cookie];
+			}
+		}
 
-		return $json_data;	
+		$json_data = json_encode( $data );
+		return $json_data;
 	}
 
 	/**
 	 * POST data to remote server
 	 * 
 	 */
-	public function post_data( $data ) {
+	public function post_data( $args ) {
 
-		wp_die( $data );
+		$public_key = get_option(LF_PREFIX . 'public_key');
+		$defaults = array(
+				'method' => 'POST',
+				'headers' => array(
+						'Authorization' => 'Token ' . $public_key,
+						'Content-Type' => 'application/json',
+				),
+		);
+		
+		$args = wp_parse_args($args, $defaults);
+		
+		$response = wp_remote_request(LF_EVENTS_API, $args);
+
+		if(!is_wp_error($response) && $response['response']['code'] == 201) {
+			$body = json_decode($response['body'], true);
+			$cookie = lf_visitor_cookie($body['visitor']);
+			setcookie($cookie['name'], $cookie['value'], $cookie['expire'], $cookie['path'], $cookie['domain']);
+			$cookie = lf_session_cookie($body['session']);
+			setcookie($cookie['name'], $cookie['value'], $cookie['expire'], $cookie['path'], $cookie['domain']);
+			lf_add_lead($body['visitor'], $user_id, LF_SYNC_STATUS_OK);
+		} else {
+			if(isset($body['visitor'])) {
+				lf_add_lead($body['visitor'], $user_id, LF_SYNC_STATUS_UPDATE_PENDING);
+			}
+		}
+
 	}
 }
